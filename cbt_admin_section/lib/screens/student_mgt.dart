6938118a3a15cc7de29ec1b5fb0aed7a08b810d9
Klaus <emmanuelshaba2002@outlook.com>
2026-03-21 13:28:37 +0100

    21/02/2026 - 10:20

--- cbt_admin_section/lib/screens/student_mgt.dart
+++ cbt_admin_section/lib/screens/student_mgt.dart
@@ -2,6 +2,7 @@ import 'dart:math';
 import 'package:flutter/material.dart';
 
 import 'package:cbt_admin_section/widgets/add_students_dialog.dart';
+import 'package:cbt_admin_section/widgets/no_registered_student.dart';
 
 class Student {
   String id;
@@ -71,6 +72,7 @@ class _StudentManagementPageState extends State<StudentManagementPage> {
     return Padding(
       padding: const EdgeInsets.all(24.0),
       child: Column(
+        mainAxisSize: MainAxisSize.max,
         crossAxisAlignment: CrossAxisAlignment.start,
         children: [
           Row(
@@ -111,59 +113,70 @@ class _StudentManagementPageState extends State<StudentManagementPage> {
           const SizedBox(height: 24),
           Expanded(
             child: Card(
-              child: ListView.separated(
-                itemCount: filteredStudents.length,
-                separatorBuilder: (c, i) => const Divider(),
-                itemBuilder: (context, index) {
-                  final s = filteredStudents[index];
-                  return ListTile(
-                    leading: CircleAvatar(
-                      child: Text(s.fullName[0].toUpperCase()),
+              child: students.isEmpty
+                  ? NoRegisteredStudent()
+                  : ListView.separated(
+                      itemCount: filteredStudents.length,
+                      separatorBuilder: (c, i) => const Divider(),
+                      itemBuilder: (context, index) {
+                        final s = filteredStudents[index];
+                        return ListTile(
+                          leading: CircleAvatar(
+                            child: Text(s.fullName[0].toUpperCase()),
+                          ),
+                          title: Text(s.fullName),
+                          subtitle: Text(
+                            "${s.regNumber}\t\u2022\t${s.stdClass}",
+                          ),
+                          subtitleTextStyle: Theme.of(
+                            context,
+                          ).textTheme.labelSmall,
+                          trailing: Row(
+                            mainAxisSize: MainAxisSize.min,
+                            children: [
+                              Container(
+                                padding: const EdgeInsets.symmetric(
+                                  horizontal: 12,
+                                  vertical: 6,
+                                ),
+                                decoration: BoxDecoration(
+                                  color: Colors.blue[50],
+                                  borderRadius: BorderRadius.circular(8),
+                                  border: Border.all(color: Colors.blue),
+                                ),
+                                child: Text(
+                                  "Passcode: ${s.currentPasscode}",
+                                  style: const TextStyle(
+                                    fontWeight: FontWeight.bold,
+                                    color: Colors.blue,
+                                  ),
+                                ),
+                              ),
+                              const SizedBox(width: 10),
+                              IconButton(
+                                icon: const Icon(Icons.refresh),
+                                onPressed: () => _regeneratePasscode(index),
+                                tooltip: "Regenerate Passcode",
+                              ),
+                              IconButton(
+                                icon: const Icon(
+                                  Icons.delete,
+                                  color: Colors.red,
+                                ),
+                                onPressed: () {
+                                  setState(() {
+                                    students.removeWhere(
+                                      (item) => item.id == s.id,
+                                    );
+                                  });
+                                },
+                                tooltip: "Delete Student",
+                              ),
+                            ],
+                          ),
+                        );
+                      },
                     ),
-                    title: Text(s.fullName),
-                    subtitle: Text("${s.regNumber}\t\u2022\t${s.stdClass}"),
-                    subtitleTextStyle: Theme.of(context).textTheme.labelSmall,
-                    trailing: Row(
-                      mainAxisSize: MainAxisSize.min,
-                      children: [
-                        Container(
-                          padding: const EdgeInsets.symmetric(
-                            horizontal: 12,
-                            vertical: 6,
-                          ),
-                          decoration: BoxDecoration(
-                            color: Colors.blue[50],
-                            borderRadius: BorderRadius.circular(8),
-                            border: Border.all(color: Colors.blue),
-                          ),
-                          child: Text(
-                            "Passcode: ${s.currentPasscode}",
-                            style: const TextStyle(
-                              fontWeight: FontWeight.bold,
-                              color: Colors.blue,
-                            ),
-                          ),
-                        ),
-                        const SizedBox(width: 10),
-                        IconButton(
-                          icon: const Icon(Icons.refresh),
-                          onPressed: () => _regeneratePasscode(index),
-                          tooltip: "Regenerate Passcode",
-                        ),
-                        IconButton(
-                          icon: const Icon(Icons.delete, color: Colors.red),
-                          onPressed: () {
-                            setState(() {
-                              students.removeWhere((item) => item.id == s.id);
-                            });
-                          },
-                          tooltip: "Delete Student",
-                        ),
-                      ],
-                    ),
-                  );
-                },
-              ),
             ),
           ),
         ],
@@ -180,6 +193,6 @@ String generatePasscode() {
 
 // 2. UPDATE MOCK DATA
 final List<Student> students = [
-  Student("1", "John Doe", "REG/2024/0001", "JSS 1", "123456", true),
-  Student("2", "Jane Smith", "REG/2024/0002", "SSS 2", "654321", true),
+  // Student("1", "John Doe", "REG/2024/0001", "JSS 1", "123456", true),
+  // Student("2", "Jane Smith", "REG/2024/0002", "SSS 2", "654321", true),
 ];
--- cbt_admin_section/lib/widgets/no_registered_student.dart
+++ cbt_admin_section/lib/widgets/no_registered_student.dart
@@ -6,8 +6,15 @@ class NoRegisteredStudent extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Column(
+      mainAxisAlignment: MainAxisAlignment.center,
+      mainAxisSize: MainAxisSize.max,
       children: [
-        Image.asset("lib/assets/images/no_student.png", fit: BoxFit.contain),
+        Image.asset(
+          "lib/assets/images/no_student.png",
+          fit: BoxFit.contain,
+          height: 200,
+          width: 200,
+        ),
         Text(
           "No registered student, click the Add Student button below to register a student.",
         ),
